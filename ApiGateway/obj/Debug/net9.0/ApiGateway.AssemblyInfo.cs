a90ba88bf10b160bbd3b15fd6c6a7878180664a5
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-09-27 19:56:38 -0300

    ajuste docker

--- ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d57271daf13b166a4c8f05bba59c611145952d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecbeec656ca4e3a0146a6fd6d0a3a7434ecccca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
